e722a1856e28acf27070cead59e5008ca6f03063
Grzegorz Lucki <grz.lucki@gmail.com>
2021-05-25 19:52:16 +0200

    Change initial sql

--- init.sql
+++ init.sql
@@ -1,1 +1,8 @@
-create table coins(name text, ticker text, link text, created_at timestamp without time zone);
+create table coins
+(
+    name text,
+    ticker text,
+    link text,
+    created_at timestamp without time zone default CURRENT_TIMESTAMP,
+    CONSTRAINT link UNIQUE (link)
+);
