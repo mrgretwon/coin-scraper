cfc86b4772f290e6573e3bd8978adbd239a78d1c
Grzegorz Lucki <grz.lucki@gmail.com>
2021-05-25 13:07:16 +0200

    Add database pipeline

--- cmc/pipelines.py
+++ cmc/pipelines.py
@@ -1,13 +1,24 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import psycopg2
+from cmc.settings import DATABASE_HOST, DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD, DATABASE_PORT
 
 
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+class DatabasePipeline(object):
 
+    def open_spider(self, spider):
+        self.connection = psycopg2.connect(
+            host=DATABASE_HOST,
+            user=DATABASE_USER,
+            password=DATABASE_PASSWORD,
+            dbname=DATABASE_NAME,
+            port=DATABASE_PORT
+        )
+        self.cur = self.connection.cursor()
 
-class CmcPipeline:
+    def close_spider(self, spider):
+        self.cur.close()
+        self.connection.close()
+
     def process_item(self, item, spider):
+        self.cur.execute('INSERT INTO twoja_stara(name) VALUES (%s)', (item['name'],))
+        self.connection.commit()
         return item
